4ab3f0291da1ff01cd19b6b1e36c08436684eea9
kalkiya <55840410+kalkiya@users.noreply.github.com>
2019-09-27 19:10:05 +0530

    Image gallery creation

---
+++ slide.js
@@ -0,0 +1,38 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
+
+function zoomImage(n) {
+	 document.getElementById("imageShow").src="../assets/image"+n+".jpg";
+     document.getElementById("zoomImage").style.display = "block";
+	setTimeout(function(){	  
+       document.getElementById("zoomImage").style.display = "none";
+    },1000);
+	
+		
+}
